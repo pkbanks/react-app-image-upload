7921a91835e3ed152fe666d1ad7f444b04cf7eb6
PK Banks <pkbanks12@gmail.com>
2019-02-20 22:58:06 -0500

    setup models and activestorage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+pk = User.create(email: "pkbanks@gmail.com")
+mario = User.create(email: "mario@getpop.co")
